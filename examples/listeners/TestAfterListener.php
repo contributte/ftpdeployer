4f5da2000c754b37c53d867c973de7bc2f16b27d
Milan Felix Sulc <rkfelix@gmail.com>
2015-05-26 11:01:05 +0200

    Examples: fix implementation

--- examples/listeners/TestAfterListener.php
+++ examples/listeners/TestAfterListener.php
@@ -3,18 +3,22 @@
 use Deployment\Deployer;
 use Deployment\Logger;
 use Deployment\Server;
+use Minetro\Deployer\Config\Config;
+use Minetro\Deployer\Config\Section;
 use Minetro\Deployer\Listeners\AfterListener;
 
 class TestAfterListener implements AfterListener
 {
 
     /**
+     * @param Config $config
+     * @param Section $section
      * @param Server $server
      * @param Logger $logger
      * @param Deployer $deployer
      * @return void
      */
-    public function onAfter(Server $server, Logger $logger, Deployer $deployer)
+    public function onAfter(Config $config, Section $section, Server $server, Logger $logger, Deployer $deployer)
     {
         $stop();
     }
--- examples/listeners/TestBeforeListener.php
+++ examples/listeners/TestBeforeListener.php
@@ -3,18 +3,22 @@
 use Deployment\Deployer;
 use Deployment\Logger;
 use Deployment\Server;
+use Minetro\Deployer\Config\Config;
+use Minetro\Deployer\Config\Section;
 use Minetro\Deployer\Listeners\BeforeListener;
 
 class TestBeforeListener implements BeforeListener
 {
 
     /**
+     * @param Config $config
+     * @param Section $section
      * @param Server $server
      * @param Logger $logger
      * @param Deployer $deployer
      * @return void
      */
-    public function onBefore(Server $server, Logger $logger, Deployer $deployer)
+    public function onBefore(Config $config, Section $section, Server $server, Logger $logger, Deployer $deployer)
     {
         $stop();
     }
